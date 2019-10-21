a5330ad1c9ec92bd7cb20fc09e20d4c2e707713e
shurooqGhaith <shurooq.ghaith97@hotmail.com>
2019-10-21 13:25:02 -0700

    Add All Files

--- screens/PatientInfo.js
+++ screens/PatientInfo.js
@@ -8,21 +8,19 @@ import {
   ImageBackground,
   Platform,
   Picker,
-  FlatList,
-  Modal,
-  TouchableHighlight,
-  Alert
+  TextInput,
+  CheckBox
 } from "react-native";
 import { Block, Text, theme } from "galio-framework";
 
-import { Button } from "../components";
+import { Button,Icon,Input } from "../components";
 import { Images, argonTheme } from "../constants";
 import { HeaderHeight } from "../constants/utils";
 import fire from "../constants/firebaseConfigrations";
 import DateTimePicker from "react-native-modal-datetime-picker";
 import MapView,{Marker} from "react-native-maps";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { Divider,Icon } from 'react-native-elements';
+import { Divider,Header } from 'react-native-elements';
 import { Menu, MenuProvider, MenuOptions, MenuOption, MenuTrigger} from "react-native-popup-menu";
 const { width, height } = Dimensions.get("screen");
 
@@ -33,14 +31,28 @@ class PatientInfo extends React.Component {
   constructor(props){
     super(props);
     this.authListener=this.authListener.bind(this);
+    this.addMedicineName=this.addMedicineName.bind(this);
+    this.handleChange=this.handleChange.bind(this);
+    this.submit=this.submit.bind(this);
         this.state={
       username:"",
       idDoctor:"",
       idPatient:'',
       nodata:false,
       info:[],
-      email:''
-      
+      email:'',
+      session:'',
+      checkedTemporary:false,
+      checkedPermanent:false,
+      checkedRepairing:false,
+      checkedWindmillDressing:false,
+      medicinesName:[],
+      medicine:'',
+      medicineNameFromPicker:'',
+      medicineNameFromDB:[],
+      noMedicine:true,
+      checkedMoney:false,
+      money:''
     }
   }
 
@@ -77,17 +89,60 @@ class PatientInfo extends React.Component {
   }
 
 
+  addMedicineName(){
+    this.setState({
+        medicinesName:[...this.state.medicinesName,""]
+    })
+  }
+
+  handleChange(e,index){
+    this.state.medicinesName[index]=e;
+    this.setState({medicinesName:this.state.medicinesName})
+        }
   
-  
+        submit(){
+	
+          var process=[];
+          if(this.state.checkedPermanent){process.push("permanent dental filling")}
+          if(this.state.checkedTemporary){process.push("temporary dental filling")}
+          if(this.state.checkedRepairing){process.push("repairing")}
+          if(this.state.checkedWindmillDressing){process.push("windmill dressing")}
+
+
+          if(!this.state.checkedMoney){this.setState({money:0})}
+        
+          fire.database().ref("users").child(this.state.idDoctor).child("session").push().set(
+          {
+            'idPatient':this.state.idPatient,
+            'sessionNumber':this.state.session,
+            'process':process,
+            'money':this.state.money,
+            'medicine':this.state.medicinesName
+            
+            
+          }
+          )
+          fire.database().ref("users").child(this.state.idPatient).child("session").push().set(
+            {
+              'idDoctor':this.state.idDoctor,
+              'sessionNumber':this.state.session,
+              'process':process,
+              'money':this.state.money,
+              'medicine':this.state.medicinesName
+              
+              
+            }
+            )
+            //اعمل الموعد هاد متوفر
+        }
+
+
   render() {
     return (
       <Block flex style={styles.profile}>
         <Block flex>
-          <ImageBackground
-            source={Images.ProfileBackground}
-            style={styles.profileContainer}
-            imageStyle={styles.profileBackground}
-          >
+         
+          
             <ScrollView
               showsVerticalScrollIndicator={false}
               style={{ width, marginTop: '25%' }}
@@ -99,10 +154,101 @@ class PatientInfo extends React.Component {
                   <Text>{this.state.email}</Text>
                   </Block>
                   
-                  
+                  <View style={{flexDirection:'column',marginTop:150,marginLeft:50}}>
+                   
+                       <TextInput  
+                              placeholder="Enter session Number"  
+                              underlineColorAndroid='transparent'  
+                             style={styles.TextInputStyle}  
+                             onChangeText={value => this.setState({session:value})}
+                             keyboardType = 'numeric'
+                              />
+
+                              <View style={{flexDirection:'column',borderRadius: 10, borderWidth: 1, borderColor: '#009688'}}>
+                              <View style={{flexDirection:'row' }}>
+                              <CheckBox
+                                       value={this.state.checkedTemporary}
+                                       onValueChange={() => this.setState({ checkedTemporary: !this.state.checkedTemporary })}
+                                 />
+                                  <Text style={{marginTop: 5}}> Temporary dental filling</Text>
+                                  </View>
+                                  <View style={{flexDirection:'row'}}>
+                                  <CheckBox
+                                       value={this.state.checkedPermanent}
+                                       onValueChange={() => this.setState({ checkedPermanent: !this.state.checkedPermanent })}
+                                 />
+                                  <Text style={{marginTop: 5}}> Permanent dental filling</Text>
+                                  </View>
+                              
+                              <View style={{flexDirection:'row'}}>
+                              <View style={{flexDirection:'row'}}>
+                              <CheckBox
+                                       value={this.state.checkedRepairing}
+                                       onValueChange={() => this.setState({ checkedRepairing: !this.state.checkedRepairing })}
+                                 />
+                                  <Text style={{marginTop: 5}}> Repairing</Text>
+                                  </View>
+                                  <View style={{flexDirection:'row'}}>
+                                  <CheckBox
+                                       value={this.state.checkedWindmillDressing}
+                                       onValueChange={() => this.setState({ checkedWindmillDressing: !this.state.checkedWindmillDressing })}
+                                 />
+                                  <Text style={{marginTop: 5}}> Windmill Dressing</Text>
+                                  </View>
+                              </View>
+                              </View>
+                              <View style={{flexDirection:'column'}}>
+                             <View style={{flexDirection:'row',marginLeft:50}}>
+                             <CheckBox
+                                       value={this.state.checkedMoney}
+                                       onValueChange={() => this.setState({ checkedMoney: !this.state.checkedMoney })}
+                                 />
+                                  <Text style={{marginTop: 5}}>Paid </Text>
+                                  
+                                  { this.state.checkedMoney &&<TextInput
+                                  style={styles.input}  
+                         keyboardType = 'numeric'
+                        placeholder="Amount paid"
+                        onChangeText={(money)=>this.setState({money})}
+                        value={this.state.money}
+                      /> }
+                             </View>
+                             </View>
+                              {this.state.medicinesName.map((name,index)=>{
+                      return(
+                          <View key={index} style={{marginLeft:50,width:width*0.5}}>
+                          <Input
+                        
+                        borderless
+                        placeholder="medicine name"
+                        onChangeText={(Mname)=>this.handleChange(Mname,index)}
+                        value={name}
+                        iconContent={
+                            <Icon
+                            size={16}
+                            color={argonTheme.COLORS.ICON}
+                            name="shop"
+                            family="ArgonExtra"
+                            style={styles.inputIcons}
+                          />
+                          
+                        }
+                      />
+                          </View>
+                      )
+                    })}
+
+                    <Button
+                      style={{ backgroundColor: argonTheme.COLORS.GRADIENT_START,marginTop:10, width: width * 0.5,borderRadius:10,marginLeft:50}}
+                      onPress={this.addMedicineName}
+                    >
+                     add medicine name
+                    </Button>
+                    </View>
+
                
             </ScrollView>
-          </ImageBackground>
+          
         </Block>
       </Block>
     );
@@ -117,7 +263,8 @@ const styles = StyleSheet.create({
   profile: {
     marginTop: Platform.OS === "android" ? -HeaderHeight : 0,
     // marginBottom: -HeaderHeight * 2,
-    flex: 1
+    flex: 1,
+    backgroundColor:'#eee'
   },
   profileContainer: {
     width: width,
@@ -130,9 +277,24 @@ const styles = StyleSheet.create({
     height: height / 2
   },
   
-  
-  
-  
+  TextInputStyle: {  
+    textAlign: 'center',  
+    height: 40,  
+    borderRadius: 10,  
+    borderWidth: 2,  
+    borderColor: '#009688',  
+    marginBottom: 10  ,
+    width:width*0.5,
+    marginLeft:20
+} ,
+
+input: {
+  borderRadius: 4,
+  borderColor: argonTheme.COLORS.BORDER,
+  height: 44,
+  backgroundColor: '#FFFFFF',
+  marginTop:10
+},
   
 });
 
