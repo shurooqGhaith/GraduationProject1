36800ee4bcfc18cdd11dfc44096add8011f9da17
shurooqGhaith <shurooq.ghaith97@hotmail.com>
2019-11-08 03:19:52 -0800

    Add All Files

--- screens/ShowAllLocation.js
+++ screens/ShowAllLocation.js
@@ -15,6 +15,7 @@ class ShowAllLocation extends Component {
     this.deg2Rad=this.deg2Rad.bind(this);
     this.pythagorasEquirectangular =this.pythagorasEquirectangular.bind(this);
     this.NearestCity=this.NearestCity.bind(this);
+    this.retrieveData=this.retrieveData.bind(this);
     this.state = {
         id:'',
         clinicNames:[],
@@ -34,103 +35,89 @@ class ShowAllLocation extends Component {
        }
 };
 
+retrieveData(){
+
+  const { navigation } = this.props;  
+  var id=navigation.getParam('id');
+  this.setState({
+      id:id
+  });
+  fire.database().ref("users").child(id).child("latitude").on('value',(snap)=>{
+    this.setState({latitude:snap.val()})
+  })
+
+  fire.database().ref("users").child(id).child("longitude").on('value',(snap)=>{
+    this.setState({longitude:snap.val()})
+  })
+
+       var array=[];
+       fire.database().ref("users").on('value',(snap)=>{
+          var data=snap.val();
+          var keys=Object.keys(data);
+          for(var i=0 ; i<keys.length;i++){
+
+      
+              fire.database().ref("users").child(keys[i]).child("type").on('value',(snapshot)=>{
+
+                  var app=snapshot.val();//type of user
+                  if(app=="doctor"){
+
+                      fire.database().ref("users").child(keys[i]).child("clinicName").on('value',(result)=>{
+                          if(result.val()){
+
+                              let names = Object.values(result.val());
+                              this.setState({clinicNames:names})
+
+                              this.state.clinicNames.map((value,index)=>{
+                                array.push({clinicName:value.clinic,latitude:value.latitude,longitude:value.longitude});
+                              })
+                          }
+                      }) //clinic names fire
+                  }//doctor if
+              })
+
+          }//keys for
+         // alert(array.length);//8
+
+          var result = array.reduce((unique, o) => {
+              if(!unique.some(obj => obj.clinicName === o.clinicName )) {
+                unique.push(o);
+              }
+              return unique;
+          },[]);
+         // alert(result.length);//4
+          
+         var minDif = 99999;
+      var closest;
+    
+           // for (var index = 0; index < array.length; ++index) {
+             result.map((location,index)=>{
+                   // alert(location.latitude+"\n"+location.longitude+"\n"+location.clinicName)//طبعهم
+                   var lat1 = this.state.latitude * Math.PI / 180;
+                   var lat2 = location.latitude * Math.PI / 180;
+                   var lon1 = this.state.longitude * Math.PI / 180;
+                   var lon2 = location.longitude * Math.PI / 180;
+                    var R = 6371; // km
+                    var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+                    var y = (lat2 - lat1);
+                    var d = Math.sqrt(x * x + y * y) * R;
+                     if (d < minDif) {
+                          closest =location.clinicName;
+                           minDif = d;
+                           }
+             })
+
+             alert(closest);
+             this.setState({
+              clinics:result,
+              nodata:false
+          })
+       })
+
+}
    
 	componentDidMount() {
-    const { navigation } = this.props;  
-    var id=navigation.getParam('id');
-    this.setState({
-        id:id
-    });
-    fire.database().ref("users").child(id).child("latitude").on('value',(snap)=>{
-      this.setState({latitude:snap.val()})
-    })
-
-    fire.database().ref("users").child(id).child("longitude").on('value',(snap)=>{
-      this.setState({longitude:snap.val()})
-    })
-
-         var array=[];
-         fire.database().ref("users").on('value',(snap)=>{
-            var data=snap.val();
-            var keys=Object.keys(data);
-            for(var i=0 ; i<keys.length;i++){
-
-                fire.database().ref("users").child(keys[i]).child("type").on('value',(snapshot)=>{
-
-                    var app=snapshot.val();//type of user
-                    if(app=="doctor"){
-
-                        fire.database().ref("users").child(keys[i]).child("clinicName").on('value',(result)=>{
-                            if(result.val()){
-
-                                let names = Object.values(result.val());
-                                this.setState({clinicNames:names})
-
-                                this.state.clinicNames.map((value,index)=>{
-                                  array.push({clinicName:value.clinic,latitude:value.latitude,longitude:value.longitude});
-                                })
-                            }
-                        }) //clinic names fire
-                    }//doctor if
-                })
-
-            }//keys for
-           // alert(array.length);//8
-
-            var result = array.reduce((unique, o) => {
-                if(!unique.some(obj => obj.clinicName === o.clinicName )) {
-                  unique.push(o);
-                }
-                return unique;
-            },[]);
-            alert(result.length);//4
-            
-           var minDif = 99999;
-        var closest;
-      
-             // for (var index = 0; index < array.length; ++index) {
-               result.map((location,index)=>{
-                     // alert(location.latitude+"\n"+location.longitude+"\n"+location.clinicName)//طبعهم
-                     var lat1 = this.state.latitude * Math.PI / 180;
-                     var lat2 = location.latitude * Math.PI / 180;
-                     var lon1 = this.state.longitude * Math.PI / 180;
-                     var lon2 = location.longitude * Math.PI / 180;
-                      var R = 6371; // km
-                      var x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
-                      var y = (lat2 - lat1);
-                      var d = Math.sqrt(x * x + y * y) * R;
-                       if (d < minDif) {
-                            closest =location.clinicName;
-                             minDif = d;
-                             }
-               })
-
-              // alert(closest);
-               this.setState({
-                clinics:result,
-                nodata:false
-            })
-    
-
-         })
-         
-        
-
-         if(array.length>0){
-            // var result = array.reduce((unique, o) => {
-            //     if(!unique.some(obj => obj.clinicName === o.clinicName )) {
-            //       unique.push(o);
-            //     }
-            //     return unique;
-            // },[]);
-            this.setState({
-                clinics:array,
-                nodata:false
-            })
-        }
-    
-	
-	
+       this.retrieveData();	
   }
   deg2Rad (deg) {
   return deg * Math.PI / 180;
@@ -156,10 +143,9 @@ onRegionChange(region){
 }	
 
 render () {
+ // if(!this.state.clinics){this.retrieveData();}
 	return (
 		  <View style={styles.container} >
-
-
 
 <MapView
            style={styles.mapStyle}
